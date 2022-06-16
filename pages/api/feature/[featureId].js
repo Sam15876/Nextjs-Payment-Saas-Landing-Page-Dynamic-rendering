6e05f36478bd0118d24feab9a5ed4790bd339477
Sam Foltin <sam.foltin@gmail.com>
2022-06-16 20:25:42 +0100

    Add API endpoint for fetching specific feature by its index

---
+++ pages/api/feature/[featureId].js
@@ -0,0 +1,9 @@
+export default function handler(req, res) {
+    const data = [
+        { title: 'Send Money', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui id lobortis pretium blandit.' },
+        { title: 'Money Reqyest', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui id lobortis pretium blandit.' },
+        { title: 'Pay Bill', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui id lobortis pretium blandit.' },
+        { title: 'Mobile Recharge', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui id lobortis pretium blandit.' }
+    ]
+    res.status(200).json(data.at(req.query.featureId))
+}
