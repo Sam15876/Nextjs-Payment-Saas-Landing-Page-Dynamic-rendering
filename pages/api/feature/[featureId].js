39c2ae67520d8aac62dfa1c25080b3837b34b9f6
Sam Foltin <sam.foltin@gmail.com>
2022-06-16 21:29:07 +0100

    Fix out-of-bounds issue where range of features would be 0-3 instead of 1-4

--- pages/api/feature/[featureId].js
+++ pages/api/feature/[featureId].js
@@ -5,5 +5,5 @@ export default function handler(req, res) {
         { title: 'Pay Bill', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui id lobortis pretium blandit.' },
         { title: 'Mobile Recharge', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui id lobortis pretium blandit.' }
     ]
-    res.status(200).json(data.at(req.query.featureId))
+    res.status(200).json(data.at(req.query.featureId - 1))
 }
