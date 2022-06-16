f5e8c655b8f135bab6c986731d0647d4823e1f64
Sam Foltin <sam.foltin@gmail.com>
2022-06-16 22:13:41 +0100

    Remove netlify deploy fixes since it doesn't work anyway

--- pages/feature/[featureId].js
+++ pages/feature/[featureId].js
@@ -24,23 +24,8 @@ export async function getStaticPaths() {
 
 export async function getStaticProps(context) {
     const featureId = context?.params?.featureId
-    
-    let feature
-    
-    // Need to run a try-catch block because this will fail on PROD build as API won't be running at the time of build and hence fetch attempt will fail
-    try {
-        const response = await fetch(`${process.env.BASE_URL}/api/feature/${featureId}`)
-        feature = await response.json()
-    }
-    catch(error) {
-        const features = [
-            { title: 'Send Money', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui id lobortis pretium blandit.' },
-            { title: 'Money Request', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui id lobortis pretium blandit.' },
-            { title: 'Pay Bill', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui id lobortis pretium blandit.' },
-            { title: 'Mobile Recharge', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui id lobortis pretium blandit.' }
-        ]
-        feature = features.at(featureId - 1)
-    }
+    const response = await fetch(`/api/feature/${featureId}`)
+    const feature = await response.json()
 
     return {
         props: {
