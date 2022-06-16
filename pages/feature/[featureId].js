da6469abe9bdc5e0ec4d3417dad2038843a7d480
Sam Foltin <sam.foltin@gmail.com>
2022-06-16 22:02:19 +0100

    Overriding SSG with static non-fetched data as api is offline during netlify deployment

--- pages/feature/[featureId].js
+++ pages/feature/[featureId].js
@@ -24,8 +24,23 @@ export async function getStaticPaths() {
 
 export async function getStaticProps(context) {
     const featureId = context?.params?.featureId
-    const response = await fetch(`${process.env.BASE_URL}/api/feature/${featureId}`)
-    const feature = await response.json()
+    
+    let feature
+    
+    // Need to run a try-catch block because this will fail on PROD build as API won't be running at the time of build and hence fetch attempt will fail
+    try {
+        const response = await fetch(`${process.env.BASE_URL}/api/feature/${featureId}`)
+        feature = await response.json()
+    }
+    catch(error) {
+        const features = [
+            { title: 'Send Money', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui id lobortis pretium blandit.' },
+            { title: 'Money Request', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui id lobortis pretium blandit.' },
+            { title: 'Pay Bill', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui id lobortis pretium blandit.' },
+            { title: 'Mobile Recharge', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui id lobortis pretium blandit.' }
+        ]
+        feature = features.at(featureId - 1)
+    }
 
     return {
         props: {
