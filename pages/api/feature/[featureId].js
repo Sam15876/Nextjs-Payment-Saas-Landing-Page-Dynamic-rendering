a6eea3103e0f923c8c8ff3a050fdea83f3104ccf
Sam Foltin <sam.foltin@gmail.com>
2022-06-16 20:32:52 +0100

    Fix typo

--- pages/api/feature/[featureId].js
+++ pages/api/feature/[featureId].js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     const data = [
         { title: 'Send Money', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui id lobortis pretium blandit.' },
-        { title: 'Money Reqyest', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui id lobortis pretium blandit.' },
+        { title: 'Money Request', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui id lobortis pretium blandit.' },
         { title: 'Pay Bill', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui id lobortis pretium blandit.' },
         { title: 'Mobile Recharge', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui id lobortis pretium blandit.' }
     ]
