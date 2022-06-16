4c5e18856408c3a9be480671ac1fc02543c88dc7
Sam Foltin <sam.foltin@gmail.com>
2022-06-16 21:10:20 +0100

    Display number of downloads in navigation bar and call increment api route on click

--- components/global/pageContainer/NavigationBar.js
+++ components/global/pageContainer/NavigationBar.js
@@ -2,10 +2,30 @@
 import Link from 'next/link'
 import Image from 'next/image'
 
+// React
+import { useState, useEffect } from 'react'
+
 // Images
 import logo from '../../../public/payLogo.svg'
 
 export default function NavigationBar() {
+    const [downloadCount, setDownloadCount] = useState()
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch('/api/downloads')
+            const data = await response.json()
+            setDownloadCount(data ?? 0)
+        }
+
+        fetchData()
+    }, [])
+
+    async function handleDownloadClick() {
+        const response = await fetch('/api/downloads/increment')
+        const data = await response.json()
+        setDownloadCount(data)
+    }
+
     return (
         <nav className='absolute top-0 left-0 w-full flex justify-around items-center z-10 pt-12 px-36'>
 
@@ -20,7 +40,12 @@ export default function NavigationBar() {
 
             {/* Download button */}
             <div className='basis-1/2 text-[14px] text-[#191D2A] flex justify-end'>
-                <Link href='#'><a className='p-4 rounded-xl font-semibold bg-white'>DOWNLOAD</a></Link>
+                <button onClick={handleDownloadClick}>
+                    <a className='px-4 py-2 rounded-xl bg-white flex flex-col gap-y-1'>
+                        <div className='font-semibold'>DOWNLOAD</div>
+                        { downloadCount !== undefined && <div className='text-xs'><span className='text-secondary'>{downloadCount}</span> {downloadCount === 1 ? 'download' : 'downloads'}</div> }
+                    </a>
+                </button>
             </div>
         </nav>
     )
